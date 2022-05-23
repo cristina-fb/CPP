9724534272d821e08d6250ba9298ec4166cc0178
cristina-fb <cristina_fb_26@hotmail.es>
2022-05-23 19:25:01 +0200

    cpp03 ex01 OK

---
+++ cpp3/ex01/ClapTrap.hpp
@@ -0,0 +1,30 @@
+#ifndef CLAPTRAP_CLASS_H
+# define CLAPTRAP_CLASS_H
+# include <iostream>
+
+class ClapTrap
+{
+    public:
+        ClapTrap( void );
+        ClapTrap( std::string name);
+        ClapTrap( ClapTrap & src );
+        ~ClapTrap( void );
+
+        ClapTrap & operator=( ClapTrap & src );
+
+        void attack( std::string const & target );
+        void takeDamage( unsigned int amount );
+        void beRepaired( unsigned int amount );
+
+        int getHitpoints( void );
+        int getEnergypoints( void );
+        int getDamage( void );
+        std::string getName( void );
+
+    protected:
+        std::string _name;
+        int _hitpoints;
+        int _energypoints;
+        int _damage;
+};
+#endif
--- cpp3/ex01/main.cpp
+++ cpp3/ex01/main.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 #include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int main( int argc, char** argv)
 {
     if (argc == 1)
     {
-        ClapTrap A;
-        ClapTrap B("Paco");
-        ClapTrap C = B;
+        ScavTrap A;
+        ScavTrap B("Paco");
+        ScavTrap C = B;
 
         B.attack( "Mariano" );
         B.takeDamage( 7 );
         std::cout << "Hitpoints of " << B.getName() << " are " << B.getHitpoints() << " points!" << std::endl;
-        B.takeDamage( 5 );
-        std::cout << "Hitpoints of " << B.getName() << " are " << B.getHitpoints() << " points!" << std::endl;
-        B.takeDamage( 5 );
-        std::cout << "Hitpoints of " << B.getName() << " are " << B.getHitpoints() << " points!" << std::endl;
         B.beRepaired( 8 );
         std::cout << "Hitpoints of " << B.getName() << " are " << B.getHitpoints() << " points!" << std::endl;
+        B.guardGate();
     }
     else
         std::cout << argv[0] << "doesn't accept parameters" << std::endl;
