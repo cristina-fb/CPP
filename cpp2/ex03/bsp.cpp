fee5f5b9ab4c82e49faa09f317a5159890b8fc2a
Cristina Fernandez baldovinos <crisfern@c3r1s5.42madrid.com>
2022-05-30 15:58:23 +0200

    cpp02 ex03 OK

--- cpp2/ex03/bsp.cpp
+++ cpp2/ex03/bsp.cpp
@@ -2,32 +2,73 @@
 #include "Point.hpp"
 #include "Fixed.hpp"
 
-bool isTriangle(Point const & a, Point const & b, Point const & c)
+bool getEquationParams(Point const & a, Point const & b, Fixed & M, Fixed & N)
 {
-	if (((a.getX()).getRawBits() == (b.getX()).getRawBits()) && ((c.getX()).getRawBits() == (b.getX()).getRawBits()))
+	Fixed ax(a.getX()), ay(a.getY()), bx(b.getX()), by(b.getY());
+	if(ax > bx)
 	{
-		return false;
+		Fixed auxx = ax, auxy = ay;
+		ax = bx;
+		bx = auxx;
+		ay = by;
+		by = auxy;
 	}
-	else if (((a.getY()).getRawBits() == (b.getY()).getRawBits()) && ((c.getY()).getRawBits() == (b.getY()).getRawBits()))
-		return false;
-	else if (((a.getY()).getRawBits() == (b.getY()).getRawBits()) && ((a.getX()).getRawBits() == (b.getX()).getRawBits())) //Más casos bc y ca
-		return false;
-	return true;
+	Fixed xIncr(Fixed::max(ax, bx) - Fixed::min(ax, bx)), yIncr(by - ay);
+	if (xIncr == 0)
+	{
+		M = 0;
+		N = ax;
+		return true;
+	}
+	M = yIncr / xIncr;
+	Fixed mx(M * ax);
+	N = ay - mx;
+	return false;
 }
 
-/*bool isInPlane(Point const a, Point const b)
+bool isInPlane(Fixed & M, Fixed & N, Point const & a, Point const & p, bool flag)
 {
-	Fixed slope = 
-}*/
+	Fixed ax(a.getX()), ay(a.getY()), px(p.getX()), py(p.getY());
+
+	if (flag)
+	{
+		if (((N > ax) && (N > px)) || ((N < ax) && (N < px)))
+			return true;
+		return false;
+	}
+	Fixed mx = M * ax;
+	Fixed projAY(mx + N);
+	mx = M * px;
+	Fixed projPY(mx + N);
+	if (((projAY > ay) && (projPY > py)) || ((projAY < ay) && (projPY < py)))
+		return true;
+	return false;
+}
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-	if (!isTriangle(a, b, c))
+	Fixed M, N;
+	bool res = false, flag = false;
+
+	for(int i = 0; i < 3; i++)
 	{
-		//std::cout << "The points doesn't form a triangle!" << std::endl;
-		return false;
+		switch (i)
+		{
+			case 0:
+				flag = getEquationParams(a, b, M, N);
+				res = isInPlane(M, N, point, c, flag);
+				break;
+			case 1:
+				flag = getEquationParams(b, c, M, N);
+				res = isInPlane(M, N, point, a, flag);
+				break;
+			case 2:
+				flag = getEquationParams(c, a, M, N);
+				res =  isInPlane(M, N, point, b, flag);
+				break;
+		}
+		if (!res)
+			return false;
 	}
-	std::cout << "HOLA";
-	//return (isInPlane(a, b) && isInPlane(b, c) && isInPlane(c, a));
 	return true;
 }
--- cpp2/ex03/main.cpp
+++ cpp2/ex03/main.cpp
@@ -6,14 +6,15 @@ bool bsp( Point const a, Point const b, Point const c, Point const point);
 
 int main(int argc, char **argv)
 {
-	Point a(Fixed(2.1f), Fixed(3.1f));
-	Point b(Fixed(2.1f), Fixed(3.1f));
-	Point c(Fixed(7.8f), Fixed(6.2f));
-	Point point(Fixed(4.2f), Fixed(4.2f));
+	Point a(Fixed(2), Fixed(4));
+	Point b(Fixed(2), Fixed(4));
+	Point c(Fixed(2), Fixed(4));
+	Point point(Fixed(2), Fixed(3));
 
 	if (argc != 1)
 	{
-		std::cout << argv[0] << "doesn't accept parameters" << std::endl;
+		std::cout << argv[0] << " doesn't accept parameters" << std::endl;
+		return 0;
 	}
 	if (bsp(a, b, c, point))
 	{
