314ea84a6f746c45b82f08c1e0acfc1e79eb437b
Cristina Fernandez baldovinos <cristina_fb_26@hotmail.es>
2022-12-27 13:39:24 +0100

    cpp7 ex01 only 1 typename

--- cpp7/ex01/iter.hpp
+++ cpp7/ex01/iter.hpp
@@ -6,19 +6,22 @@
 /*   By: crisfern <crisfern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 16:39:15 by crisfern          #+#    #+#             */
-/*   Updated: 2022/12/22 12:25:18 by crisfern         ###   ########.fr       */
+/*   Updated: 2022/12/27 13:31:36 by crisfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template<typename X, typename Y, typename Z>
-void iter(X * arr, Y num, Z * fun)
+template<typename T>
+void iter(T * arr, unsigned int num, T (*fun)(T))
 {
-	for(int i = 0; i < num; i++)
+	if (arr && fun)
 	{
-		arr[i] = fun(arr[i]);
+		for(unsigned int i = 0; i < num; i++)
+		{
+			arr[i] = fun(arr[i]);
+		}
 	}
 }
 #endif
--- cpp7/ex01/main.cpp
+++ cpp7/ex01/main.cpp
@@ -6,16 +6,24 @@
 /*   By: crisfern <crisfern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 16:39:13 by crisfern          #+#    #+#             */
-/*   Updated: 2022/12/22 12:30:48 by crisfern         ###   ########.fr       */
+/*   Updated: 2022/12/27 13:38:33 by crisfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "iter.hpp"
 
-int sumar1(int a)
+int sum1(int a)
 {
 	return (a+1);
+}
+char touppercase(char a)
+{
+	if((a < 123) && (a > 96))
+	{
+		return (a-32);
+	}
+	return (a);
 }
 
 int main( void )
@@ -23,13 +31,13 @@ int main( void )
 	char carr[3] = {'a', 'b', 'c'};
 	int iarr[3] = {1, 2, 3};
 
-	std::cout << "Sum 1 to Char Array: " << std::endl;
+	std::cout << "Uppercase to Char Array: " << std::endl;
 	std::cout << "Before: " << carr[0] << carr[1] << carr[2] << std::endl;
-	iter(carr, 3, &sumar1);
+	iter(carr, 3, &touppercase);
 	std::cout << "After: " << carr[0] << carr[1] << carr[2] << std::endl;
 	std::cout << "Sum 1 to Int Array: " << std::endl;
 	std::cout << "Before: " << iarr[0] << iarr[1] << iarr[2] << std::endl;
-	iter(iarr, 3, &sumar1);
+	iter(iarr, 3, &sum1);
 	std::cout << "After: " << iarr[0] << iarr[1] << iarr[2] << std::endl;
 	return 0;
 }
